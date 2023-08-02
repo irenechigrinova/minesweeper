37f0f4750816be41d2d529e60fb853a8ff656ac7
Irina Chigrinova <to.chigrinova@gmail.com>
2023-08-02 14:25:55 +0300

    gh-actions test

--- src/renders/renderTable.js
+++ src/renders/renderTable.js
@@ -4,8 +4,10 @@ export default (state) => {
         <button class="item hidden" data-value="${id}"></button>
     `);
 
+  const style = `grid-template-rows: repeat(${rows}, 35px); grid-template-columns: repeat(${columns}, 35px);`;
+
   return `
-    <div class="table border-medium-invert" style="grid-template-rows: repeat(${rows}, 35px); grid-template-columns: repeat(${columns}, 35px)">
+    <div class="table border-medium-invert" style="${style}">
         ${data.join('')}
     </div>
   `;
