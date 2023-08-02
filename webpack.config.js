4624a5527f032991aeb8b720f450c18104914781
Irina Chigrinova <to.chigrinova@gmail.com>
2023-08-02 14:28:47 +0300

    gh-actions test

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   plugins: [
     new HtmlWebpackPlugin({
